0735b9e10ec4d98dc1d305573d93f7cc4859e483
belo355 <edilson.belo@live.com>
2020-10-12 14:08:45 -0300

    a

--- src/main/java/br/com/templatex/appmvctemplate/controller/HelloController.java
+++ src/main/java/br/com/templatex/appmvctemplate/controller/HelloController.java
@@ -1,15 +1,14 @@
 package br.com.templatex.appmvctemplate.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloController {
 
     @RequestMapping("/")
-    @ResponseBody
     public String hello(){
-        return "hello";
+        return "hello is here!";
     }
 }
--- src/main/java/br/com/templatex/appmvctemplate/controller/UserController.java
+++ src/main/java/br/com/templatex/appmvctemplate/controller/UserController.java
@@ -2,25 +2,33 @@ package br.com.templatex.appmvctemplate.controller;
 
 import br.com.templatex.appmvctemplate.Repository.UserRepository;
 import br.com.templatex.appmvctemplate.model.User;
-import ch.qos.logback.core.net.SyslogOutputStream;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+@RestController
 public class UserController {
 
     @Autowired
     private UserRepository userRepository;
 
-    @GetMapping("/user")
-    public User getUser(){
+    Logger logger = LoggerFactory.getLogger(UserController.class);
 
-        User user = userRepository.getOne((long) 1);
-        System.out.println(user);
-        return user;
-
-        //TODO: resolver returno do use
-
+    @GetMapping("/users")
+    public List<User> list(){
+        List<User> users = null;
+        try {
+            users = userRepository.findAll();
+            return users;
+        }catch(Error e){
+            System.out.println(e.getMessage());
+            logger(e.getMessage());
+        }
+        return null;
     }
 }
